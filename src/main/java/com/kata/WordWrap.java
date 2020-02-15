2a1b900490f61a04fb1d5c97efb7a63217330d0d
dishantj4 <G32277@WDN00930.resnp.sysnp.shared.fortis>
2020-02-15 14:13:57 +0100

    Add main code to pass test case of wrapping phrase in new line

--- src/main/java/com/kata/WordWrap.java
+++ src/main/java/com/kata/WordWrap.java
@@ -4,8 +4,21 @@ package com.kata;
 public class WordWrap
 {
 
+    public static void main(String[] args) {
+        System.out.println(new WordWrap().wrap("hello how are you", 11));
+    }
+
 
     public String wrap(String phrase, int wrapWidth) {
-        return "hello how\nare you";
+        StringBuilder wrappedPhrase = new StringBuilder();
+        wrappedPhrase.append(phrase);
+        if (phrase.length() > wrapWidth) {
+            String insertNewLine = "\n";
+            wrappedPhrase.insert(wrapWidth - 1, insertNewLine);
+        }
+        return wrappedPhrase.toString();
     }
+
+
+
 }
