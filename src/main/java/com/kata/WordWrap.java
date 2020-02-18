197eda83a465fdbcfa2c4cd5e2ad46272a638fa7
dishantj4 <G32277@WDN00930.resnp.sysnp.shared.fortis>
2020-02-18 11:52:42 +0100

    Rearrange and Reformat code

--- src/main/java/com/kata/WordWrap.java
+++ src/main/java/com/kata/WordWrap.java
@@ -1,10 +1,7 @@
 package com.kata;
 
 
-public class WordWrap
-{
-
-
+public class WordWrap {
 
 
     public String wrap(String phrase, int wrapWidth) {
@@ -12,31 +9,26 @@ public class WordWrap
         wrappedPhrase.append(phrase);
         char insertNewLine = '\n';
         String space = " ";
-        int pointerInPhrase =0;
-        int indexValueWhenNoSpaceInPhrase = pointerInPhrase -1;
-        while (pointerInPhrase + wrapWidth < phrase.length() ) {
-            int indexOfLastSpaceInLineToWrap = phrase.lastIndexOf(space,pointerInPhrase + wrapWidth);
+        int pointerInPhrase = 0;
+        int indexValueWhenNoSpaceInPhrase = pointerInPhrase - 1;
+        while (pointerInPhrase + wrapWidth < phrase.length()) {
+            int indexOfLastSpaceInLineToWrap = phrase.lastIndexOf(space, pointerInPhrase + wrapWidth);
 
-            if (indexOfLastSpaceInLineToWrap == indexValueWhenNoSpaceInPhrase)
-            {
+            if (indexOfLastSpaceInLineToWrap == indexValueWhenNoSpaceInPhrase) {
                 wrappedPhrase.insert(wrapWidth, insertNewLine);
                 pointerInPhrase = pointerInPhrase + wrapWidth;
-            }
-
-            else
-            {
+                indexValueWhenNoSpaceInPhrase++;
+            } else {
                 wrappedPhrase.setCharAt(indexOfLastSpaceInLineToWrap, insertNewLine);
                 pointerInPhrase = indexOfLastSpaceInLineToWrap + 1;
-                indexValueWhenNoSpaceInPhrase++;
+
 
             }
-
 
 
         }
         return wrappedPhrase.toString();
     }
-
 
 
 }
--- src/test/java/com/kata/WordWrapTest.java
+++ src/test/java/com/kata/WordWrapTest.java
@@ -7,34 +7,28 @@ import com.kata.WordWrap;
 import org.junit.Test;
 
 
-public class WordWrapTest
-{
-
+public class WordWrapTest {
 
 
     @Test
-    public void shouldReturnPhraseInNewLineWhenPhraseExceedsMaxLength()
-    {
+    public void shouldReturnPhraseInNewLineWhenPhraseExceedsMaxLength() {
         assertEquals(new WordWrap().wrap("hello how are you", 11), "hello how\nare you");
     }
 
 
     @Test
-    public void shouldReturnPhraseInOneLineWhenPhraseIsLessThanMaxLength()
-    {
+    public void shouldReturnPhraseInOneLineWhenPhraseIsLessThanMaxLength() {
         assertEquals(new WordWrap().wrap("how are you", 11), "how are you");
 
     }
 
     @Test
-    public void shouldReturnWrappedPhraseEvenWhenThereIsNoSpace()
-    {
+    public void shouldReturnWrappedPhraseEvenWhenThereIsNoSpace() {
         assertEquals(new WordWrap().wrap("Shakespeare", 9), "Shakespea\nre");
     }
 
     @Test
-    public void shouldReturnWrappedPhraseForEveryLineInPhrase()
-    {
+    public void shouldReturnWrappedPhraseForEveryLineInPhrase() {
         assertEquals(new WordWrap().wrap("hi hello how are you, I am good thank you", 9), "hi hello\nhow are\nyou, I am\ngood\nthank you");
     }
 }
